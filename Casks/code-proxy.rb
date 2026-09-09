b511974cc3847273f44cee93f5c2f2ee28107e98
Cals Ranna <calsranna@gmail.com>
2026-09-09 23:27:00 +0800

    Add code-proxy 3.1.3

--- Casks/code-proxy.rb
+++ Casks/code-proxy.rb
@@ -1,7 +1,7 @@
 cask "code-proxy" do
-  version "3.1.2"
-  sha256 "c313468351cdee25401692ae9e92bf07be33432f6ef1a93c64048f85fcbb67be"
-  url "https://github.com/CalsRanna/code_proxy/releases/download/v3.1.2/CodeProxy-macOS.zip"
+  version "3.1.3"
+  sha256 "30193d801534bc858f9314c0f5c3f310e2473cfc771c0f217fbee2157c2498fb"
+  url "https://github.com/CalsRanna/code_proxy/releases/download/v3.1.3/CodeProxy-macOS.zip"
   name "Code Proxy"
   desc "Anthropic API proxy manager for Claude Code. Supports macOS, Windows, and Linux."
   homepage "https://github.com/CalsRanna/code_proxy"
